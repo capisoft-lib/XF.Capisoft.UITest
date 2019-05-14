c9347f2663d6949dad35b568c75d3c41b7e1df14
CYU <cyril.chateau@ext.ubp.ch>
2019-05-14 15:52:50 +0200

    Fix project type

---
+++ XF.Capisoft.UITestOld/AppInitializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Utils;
+
+namespace XF.Capisoft.UITest
+{
+    public class AppInitializer
+    {
+
+        const string IpaPath = "../../../Binaries/TestingTest.ipa";
+        const string DeviceUDID = "743B1E5D-6677-4267-A024-9369B2DA2A38";
+        const string AppBundle = "com.my.TestingTest";
+
+
+        static IApp app;
+        public static IApp App
+        {
+            get
+            {
+                if (app == null)
+                    throw new NullReferenceException("'AppManager.App' not set. Call 'AppManager.StartApp()' before trying to access it.");
+                return app;
+            }
+        }
+
+        static Platform? platform;
+        public static Platform Platform
+        {
+            get
+            {
+                if (platform == null)
+                    throw new NullReferenceException("'AppManager.Platform' not set.");
+                return platform.Value;
+            }
+
+            set
+            {
+                platform = value;
+            }
+        }
+
+        public static IApp StartApp()
+        {
+            if (platform == Platform.Android)
+            {
+                app = ConfigureApp.Android.StartApp();
+            }
+            else if (platform == Platform.iOS)
+            {
+                var config = ConfigureApp.iOS;
+                config = config.Debug();
+                config = config.PreferIdeSettings();
+                config = config.DeviceIdentifier(DeviceUDID);
+                config = config.AppBundle(AppBundle);
+                config = config.WaitTimes(new WaitTimes());
+                app = config.StartApp();
+            }
+            return app;
+        }
+    }
+}
---
+++ XF.Capisoft.UITestOld/BaseTestFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace XF.Capisoft.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class BaseTestFixture
+    {
+        protected IApp app => AppInitializer.App;
+        protected bool OnAndroid => AppInitializer.Platform == Platform.Android;
+        protected bool OniOS => AppInitializer.Platform == Platform.iOS;
+
+        protected BaseTestFixture(Platform platform)
+        {
+            AppInitializer.Platform = platform;
+        }
+
+        [SetUp]
+        public virtual void BeforeEachTest()
+        {
+            AppInitializer.StartApp();
+        }
+
+        protected void LoginSuccess()
+        {
+            Login("Username", "Password");
+        }
+
+        protected void Login(string username, string password = null)
+        {
+            //new LoginPage().LoginWithCredentials(username, password);
+        }
+        // You can edit this file to define functionality that is common across many or all tests.
+        // For example, you could add a method here to log in or dismiss a welcome dialogue.
+    }
+}
